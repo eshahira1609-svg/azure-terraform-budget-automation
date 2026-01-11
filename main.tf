832d2fb791bdcab97c194dc629960eaf67f3eb36
eshahira1609-svg <e.shahira1609@gmail.com>
2026-01-11 18:01:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,8 +15,13 @@ module "budgets" {
   source = "./modules/budget"
 
   # Iterate over all target subscriptions
-  for_each = toset(var.subscription_ids)
+  for_each = var.subscription_budgets
 
   # Pass the current subscription to the module
-  subscription_id = each.value
+  subscription_id  = each.key
+  amount           = each.value.amount
+  start_date       = each.value.start_date
+  contact_emails   = each.value.contact_emails
+  budget_name      = "monthly-budget-${each.key}" # optional, unique per subscription
+
 }
