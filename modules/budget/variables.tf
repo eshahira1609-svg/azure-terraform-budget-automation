d1ad41a0a889a0d40af86bf3780d6bb8306c65e3
eshahira1609-svg <e.shahira1609@gmail.com>
2026-01-11 17:40:38 +0100

    Update variables.tf

--- modules/budget/variables.tf
+++ modules/budget/variables.tf
@@ -1,3 +1,34 @@
+# Azure subscription ID where the budget will be applied
+# Example: "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 variable "subscription_id" {
-    type = string 
-} 
+  type        = string
+  description = "Azure subscription ID where the budget is applied"
+}
+
+# Name of the budget to create
+# This should be unique per subscription
+variable "budget_name" {
+  type        = string
+  description = "Name of the budget"
+}
+
+# Monthly budget amount in EUR (or your currency)
+# Example: 500
+variable "amount" {
+  type        = number
+  description = "Monthly budget amount"
+}
+
+# Budget start date in YYYY-MM-DD format
+# Example: "2026-01-01"
+variable "start_date" {
+  type        = string
+  description = "Budget start date in YYYY-MM-DD format"
+}
+
+# Email addresses to receive budget alerts
+# Can be multiple addresses, e.g. ["user1@org.com", "user2@org.com"]
+variable "contact_emails" {
+  type        = list(string)
+  description = "Email addresses for budget notifications"
+}
