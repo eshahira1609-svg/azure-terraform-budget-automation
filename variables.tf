f38cff381dc0d9b43259ab0d27f72bcca2b18cce
eshahira1609-svg <e.shahira1609@gmail.com>
2026-01-11 17:57:34 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,8 @@
-variable "subscription_ids" {
-  type        = list(string)
- 
+variable "subscription_budgets" {
+  type = map(object({
+    amount         = number
+    start_date     = string
+    contact_emails = list(string)
+  }))
+  description = "Map of subscriptions to budgets and their details"
 }
